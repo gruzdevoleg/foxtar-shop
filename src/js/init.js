dc39bae8edf5e15a47d47e63419363ef5b2fcaac
Oleg Gruzdev <oleg.gruzdev.1982@gmail.com>
2018-12-04 14:46:58 +0200

    add form

---
+++ src/js/init.js
@@ -0,0 +1,13 @@
+;(function ($) {
+    // Init slick nav
+    $('.navigation__nav').slicknav({
+         appendTo: ".container .navigation__inner",
+        label: ''
+    });
+
+})(jQuery);
+
+$(function() {
+	//Init tabs
+	$(".tabs").tabs();
+});
